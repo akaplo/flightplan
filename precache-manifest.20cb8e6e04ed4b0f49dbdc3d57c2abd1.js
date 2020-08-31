6deb6a41f039fbbaa1325fe716292b7577e7602b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-31 02:23:29 +0000

    Deploy akaplo/flightplan to github.com/akaplo/flightplan.git:gh-pages

---
+++ precache-manifest.20cb8e6e04ed4b0f49dbdc3d57c2abd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b84f514a53cf0d66d5d833d7ecad8d9c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9aa7d1057c428c773a7d",
+    "url": "./static/css/main.2cbfd305.chunk.css"
+  },
+  {
+    "revision": "9d34b11b69c47799709b",
+    "url": "./static/js/2.e60eb419.chunk.js"
+  },
+  {
+    "revision": "5251dc1e619fc8e5812749ec31599bfe",
+    "url": "./static/js/2.e60eb419.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aa7d1057c428c773a7d",
+    "url": "./static/js/main.abec5197.chunk.js"
+  },
+  {
+    "revision": "57ccd2ed4024c83a67e0",
+    "url": "./static/js/runtime-main.41d136e1.js"
+  }
+]);
